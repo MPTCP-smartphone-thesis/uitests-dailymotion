100d9cc7a89eb0c25b0c65183bb4499466c769e7
Quentin De Coninck <quentin.deconinck@student.uclouvain.be>
2014-11-19 10:59:21 +0100

    [IMP] Rewritten code

--- src/dailymotion/LaunchSettings.java
+++ src/dailymotion/LaunchSettings.java
@@ -11,16 +11,13 @@ import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 public class LaunchSettings extends UiAutomatorTestCase {
 
 	private static String ID_VIDEO_LIST = "com.dailymotion.dailymotion:id/channelListView";
-	private static String ID_VIDEO_CARD = "com.dailymotion.dailymotion:id/rootLayout";
 	private static String ID_SEARCH = "com.dailymotion.dailymotion:id/searchItem";
 	private static String ID_TEXT = "com.dailymotion.dailymotion:id/search_src_text";
 
-	public void testDemo() throws UiObjectNotFoundException {
-		assertTrue("OOOOOpps",
- Utils.openApp(this, "Dailymotion",
-				"com.dailymotion.dailymotion"));
-		sleep(3000);
+	private static int MAX_VIDEOS = 10;
+	private static int TIME_VIDEO = 30000;
 
+	private void goToThePlaylist() {
 		UiScrollable list = Utils.getScrollableWithId(ID_VIDEO_LIST);
 
 		if (!list.exists()) {
@@ -29,56 +26,50 @@ public class LaunchSettings extends UiAutomatorTestCase {
 		}
 
 		sleep(1000);
-
-		Utils.click(Utils
-.getObjectWithId(ID_SEARCH));
-
+		Utils.click(Utils.getObjectWithId(ID_SEARCH));
 		sleep(1000);
-
 		Utils.setText(Utils.getObjectWithId(ID_TEXT), "conscience-tranquille");
-
 		getUiDevice().pressEnter();
-
 		sleep(1000);
-
 		Utils.click(Utils.getObjectWithClassName(
 				"android.widget.RelativeLayout", 0));
-
 		sleep(1000);
-
 		Utils.click(new UiObject(new UiSelector().textContains("playlists")));
-
 		sleep(1000);
-
 		Utils.click(Utils.getObjectWithClassName(
 				"android.widget.RelativeLayout", 7));
-
 		sleep(1000);
+	}
 
-		// assertTrue(Utils.clickAndWaitForNewWindow(Utils
-		// .getObjectWithDescription("Open menu")));
-		//
-		// assertTrue(Utils.click(Utils.getObjectWithClassName(
-		// "android.widget.RelativeLayout", 1)));
-		//
-		// sleep(5000);
+	private void seeVideos() {
+		UiScrollable list = Utils.getScrollableWithId(ID_VIDEO_LIST);
 
-		Utils.launchTcpdump("dailymotion", 900);
+		if (!list.exists()) {
+			getUiDevice().pressBack();
+			list = Utils.getScrollableWithId(ID_VIDEO_LIST);
+		}
+
 		int nVid = 0;
 		while (nVid < 10 && Utils.scrollForward(list)) {
 			int nb_childs = Utils.getChildCount(list);
 
-			for (int i = 0; nVid < 10 && i < nb_childs - 1; i++, nVid++) {
-				// assertTrue("Cannot see video...",
-				// Utils.click(Utils.getObjectWithId(ID_VIDEO_CARD, i)));
+			for (int i = 0; nVid < MAX_VIDEOS && i < nb_childs - 1; i++, nVid++) {
 				assertTrue("Cannot see video...", Utils.click(Utils
 						.getObjectWithClassName(
 								"android.widget.RelativeLayout", i)));
-				sleep(30000);
+				sleep(TIME_VIDEO);
 				getUiDevice().pressBack();
 			}
 		}
-		Utils.killTcpdump();
 	}
 
+	public void testDemo() throws UiObjectNotFoundException {
+		assertTrue("OOOOOpps", Utils.openApp(this, "Dailymotion",
+				"com.dailymotion.dailymotion"));
+		sleep(3000);
+		goToThePlaylist();
+		Utils.launchTcpdump("dailymotion", 900);
+		seeVideos();
+		Utils.killTcpdump();
+	}
 }
